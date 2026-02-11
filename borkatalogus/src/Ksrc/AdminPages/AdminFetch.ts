95c744523168d6438485d903236f9523d7441dbb
HKristof25 <21.hajdu.kristof@mechwart.com>
2026-02-11 07:57:53 +0100

    link update

--- borkatalogus/src/Ksrc/AdminPages/AdminFetch.ts
+++ borkatalogus/src/Ksrc/AdminPages/AdminFetch.ts
@@ -1,53 +1,10 @@
-import axios from "axios"
-
-const BaseUrl =  "https://p9qpdn6c-7072.euw.devtunnels.ms/"
-
-const protectedAdminAPI = axios.create({baseURL: BaseUrl})
-
-protectedAdminAPI.interceptors.request.use(config => {
-    const token = localStorage.getItem("token")
-    if(token)
-    {
-        config.headers["Authorization"] = `Bearer ${token}`
-    }
-    return config
-})
-
-protectedAdminAPI.interceptors.response.use(
-    (response) => {return response},
-    async (error) => {
-        const originalRequest = error.config
-
-        if(error.response?.status === 401 && !originalRequest.retryRequest)
-        {
-            originalRequest.retryRequest = true
-
-            try {
-                const refreshToken = localStorage.getItem("refresh")
-                const response = await axios.post(BaseUrl + "/api/accounts/token/refresh/",
-                    {refresh: refreshToken})
-
-                const newAccessToken = response.data.access
-                originalRequest.headers["Authorization"] = `Bearer ${newAccessToken}`
-                localStorage.setItem("token", newAccessToken)
-                console.log("Access token updated!")
-
-                return protectedAdminAPI(originalRequest)
-
-            } catch (error) {
-                console.log(error)
-                localStorage.removeItem("token")
-            }
-        }
-
-        return Promise.reject(error)
-    }
-)
+import { protectedAPI } from "../../MServices/AccountService";
+import { BaseUrl } from "../../MServices/AccountService";
 
 export async function GetDbData(url:string) {
 
     try{
-        const response = await protectedAdminAPI.get(`${BaseUrl}${url}`)
+        const response = await protectedAPI.get(`${BaseUrl}${url}`)
         console.log(url)
         if(response.status === 200){
              return response.data
